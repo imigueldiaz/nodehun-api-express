12e8569993b7fd77e00fe5df8ca0172a66f2a9c9
Iñaki de Miguel Díaz <imigueldiaz@gmail.com>
2021-04-03 17:04:14 +0200

    Beggining support for multiple words.

--- index.js
+++ index.js
@@ -13,16 +13,30 @@ const nodehun = new Nodehun(
     fs.readFileSync(path.join(base, "index.dic"))
 );
 
-app.get("/:palabra", async(req, res) => {
+app.get("/:palabras", async(req, res) => {
     try {
-        let palabra = req.params.palabra;
-        const test = await nodehun.analyze(palabra);
-        res.send(test);
+        let palabras = req.params.palabras.split(" ").map((item) => item.trim());
+        console.log(JSON.stringify(palabras));
+
+        let response = [];
+
+        await Promise.all(
+            palabras.map(async(palabra) => {
+                let result = await analyze(palabra);
+                response.push(result);
+            })
+        );
+
+        res.send(response);
     } catch (e) {
         res.send(e);
     }
 });
 
+async function analyze(word) {
+    return await nodehun.analyze(word).then((result) => result);
+}
+
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Nodehun REST API listening at http://localhost:${port}`);
 });
