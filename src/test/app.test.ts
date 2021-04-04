1406368dcb4bee39a5b101b690857856c8ef9eba
Iñaki de Miguel Díaz <imigueldiaz@gmail.com>
2021-04-04 19:01:54 +0200

    new Chai tests added and tested.

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -1,5 +1,7 @@
+import { OrderedWord } from "./../app";
 import supertest from "supertest";
 import { ApiServer } from "../app";
+import { assert } from "chai";
 
 describe("GET /", function () {
   this.timeout("10000");
@@ -8,7 +10,36 @@ describe("GET /", function () {
     ApiServer.close(done);
   });
 
-  it("it should has status code 200", async () => {
-    return await supertest(ApiServer).get("/hola").expect(200);
+  it("it should have status code 200.", async () => {
+    return await supertest(ApiServer).get("/test").expect(200);
+  });
+
+  it("it should respond with JSON.", async () => {
+    return await supertest(ApiServer)
+      .get("/test")
+      .expect("Content-Type", /json/);
+  });
+
+  it("it should be an array", async () => {
+    return await supertest(ApiServer)
+      .get("/test")
+      .expect("Content-Type", /json/)
+      .expect((res) => {
+        assert.isArray(res.body);
+      });
+  });
+
+  it("it should contain an OrderedWord whith the same id and word.", async () => {
+    return await supertest(ApiServer)
+      .get("/test")
+      .expect("Content-Type", /json/)
+      .expect((res) => {
+        let list = res.body;
+
+        assert.notStrictEqual(
+          new OrderedWord(0, "test"),
+          new OrderedWord(list[0].orderedWord.id, list[0].orderedWord.word)
+        );
+      });
   });
 });
