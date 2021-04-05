80f9e94b2779654df88fbd9441354e12ac529786
Iñaki de Miguel Díaz <imigueldiaz@gmail.com>
2021-04-05 19:50:09 +0200

    A little refactoring to fix #1

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import fs from "fs";
 import { Nodehun } from "nodehun";
 import path from "path";
+import compression from "compression";
 
 var base = path.dirname(require.resolve("dictionary-es"));
 
@@ -35,6 +36,19 @@ export class AnalizedItem {
 
 const Api = express();
 const PORT = 3000;
+
+Api.use(compression({ filter: shouldCompress }));
+
+function shouldCompress(req: express.Request, res: express.Response) {
+  if (req.headers["x-no-compression"]) {
+    // don't compress responses with this request header
+    return false;
+  }
+
+  // fallback to standard filter function
+  return compression.filter(req, res);
+}
+
 Api.get("/:words", async (req, res) => {
   try {
     let words = req.params.words.split(" ").map((item) => item);
@@ -67,9 +81,10 @@ async function obtainResponse(orderedWords: OrderedWord[]) {
 function obtainListOfWords(words: string[]) {
   let orderedWords = new Array<OrderedWord>();
 
-  for (var [key, value] of Object.entries(words)) {
-    orderedWords.push(new OrderedWord(parseInt(key), value));
-  }
+  Object.entries(words).forEach(([key, val]) =>
+    orderedWords.push(new OrderedWord(parseInt(key), val))
+  );
+
   return orderedWords;
 }
 
